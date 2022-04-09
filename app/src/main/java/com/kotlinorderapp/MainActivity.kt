342bbc54fd60f67fc35fa320815879dfa26a5c6f
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-04-09 16:44:22 +0300

    navigation added

--- app/src/main/java/com/kotlinorderapp/MainActivity.kt
+++ app/src/main/java/com/kotlinorderapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.kotlinorderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.kotlinorderapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navFragment.navController
     }
 }
