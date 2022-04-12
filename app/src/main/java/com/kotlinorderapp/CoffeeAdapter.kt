59d203939f3088ab3cb8259f53ecbebe3d234186
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-04-12 15:23:35 +0300

    recyclerview and adapter added

---
+++ app/src/main/java/com/kotlinorderapp/CoffeeAdapter.kt
@@ -0,0 +1,37 @@
+package com.kotlinorderapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CoffeeAdapter(
+    private val coffeeName: Array<String>,
+    private val coffeePrice: Array<String>,
+    private val coffeeDescription: Array<String>,
+    private val coffeeImage: ArrayList<Int>
+) : RecyclerView.Adapter<CoffeeAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val coffeeName : TextView = itemView.findViewById(R.id.coffee_name)
+        val coffeePrice : TextView = itemView.findViewById(R.id.coffee_price)
+        val coffeeDescription : TextView = itemView.findViewById(R.id.coffee_description)
+        val coffeeImage : ImageView = itemView.findViewById(R.id.coffee_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoffeeAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.coffee_card, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CoffeeAdapter.ViewHolder, position: Int) {
+        holder.coffeeName.text = coffeeName[position]
+        holder.coffeePrice.text = coffeePrice[position]
+        holder.coffeeDescription.text = coffeeDescription[position]
+        holder.coffeeImage.setImageResource(coffeeImage[position])
+    }
+
+    override fun getItemCount() = coffeeName.size
+}
--- app/src/main/java/com/kotlinorderapp/ui/order/CoffeeMenuFragment.kt
+++ app/src/main/java/com/kotlinorderapp/ui/order/CoffeeMenuFragment.kt
@@ -5,56 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kotlinorderapp.CoffeeAdapter
 import com.kotlinorderapp.R
+import com.kotlinorderapp.databinding.FragmentCoffeeMenuBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CoffeeMenuFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CoffeeMenuFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentCoffeeMenuBinding? = null
+
+    private val binding get() = _binding!!
+
+    private val coffeeImageList = arrayListOf<Int>(
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coffee_menu, container, false)
+        _binding = FragmentCoffeeMenuBinding.inflate(inflater, container, false)
+
+        val adapter = CoffeeAdapter(
+            resources.getStringArray(R.array.coffee_names),
+            resources.getStringArray(R.array.coffee_price),
+            resources.getStringArray(R.array.coffee_description),
+            coffeeImageList
+        )
+
+        binding.recylerview.layoutManager = LinearLayoutManager(activity)
+        binding.recylerview.adapter = adapter
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CoffeeMenuFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CoffeeMenuFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
+
 }
