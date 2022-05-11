14adbcd76003df34db2bba888f782b0a8ea7ec0b
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-05-11 14:54:52 +0300

    coffee data source created

---
+++ app/src/main/java/com/kotlinorderapp/CoffeeData.kt
@@ -0,0 +1,18 @@
+package com.kotlinorderapp
+
+data class CoffeeData(
+    val coffeeImageList: ArrayList<Int> = arrayListOf(
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+        R.drawable.coffee_cup,
+    ),
+)
